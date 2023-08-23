a47bede728286e2d2f9e7f97d64144569cb7a588
rangarajan-0309 <78946575+rangarajan-0309@users.noreply.github.com>
2023-08-23 09:16:48 +0530

    Update loginPage.tsx

--- src/components/loginPage.tsx
+++ src/components/loginPage.tsx
@@ -1,84 +1,64 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-//import classNames from 'classnames';
-const Login = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
-  
-    const handleLogin = async (event:any) => {
-      event.preventDefault();
-      setErrorMessage('');
-  
-      if (!username) {
-        setErrorMessage('Username is required');
-        return;
-      }
-  
-      if (!password) {
-        setErrorMessage('Password is required');
-        return;
-      }
-  
-      try {
-        const response = await axios.post('/api/login', { username, password });
-        console.log(response.data);
-      } catch (error:any) {
-        setErrorMessage(error.message);
-      }
-    };
+import React, { useState } from "react";
+import { useStore } from "../store";
+import { useHistory } from "react-router-dom";
 
-    return (
-        <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-            <div className="max-w-md w-full space-y-8">
-                <img
-                    className="mx-auto h-12 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                    alt="Workflow"
-                />
-                <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Log in to your account</h2>
-                <form onSubmit={handleLogin}>
-                    <label className="font-extrabold text-xl text-Black group relative w-full flex justify-center py-2 px-4 space-x-4">
-                    <span className="text-gray-700">Username</span>
-                        <input
-                            type="text"
-                            id="username"
-                            value={username}
-                            onChange={(event) => setUsername(event.target.value)}
-                            className="appearance-none rounded-none relative block
-                            w-full px-3 py-2 border border-gray-300
-                            placeholder-gray-500 text-gray-900 rounded-t-md
-                            focus:outline-none focus:ring-indigo-500
-                            focus:border-indigo-500 focus:z-10 sm:text-sm"
-                            // className={classNames(
-                            //   'appearance-none rounded-none relative block',
-                            //   errorMessage && 'border-red-500',
-                            // )}
-                        />
-                    </label>
-                    <label className="font-extrabold text-xl text-Black group relative w-full flex justify-center py-2 px-4 space-x-4">
-                    <span className="text-gray-700">Password</span>
-                        <input
-                            type="password"
-                            id="password"
-                            value={password}
-                            onChange={(event) => setPassword(event.target.value)}
-                            className="appearance-none rounded-none relative block
-                            w-full px-3 py-2 border border-gray-300
-                            placeholder-gray-500 text-gray-900 rounded-t-md
-                            focus:outline-none focus:ring-indigo-500
-                            focus:border-indigo-500 focus:z-10 sm:text-sm"
-                        />
-                    </label>
-                    <button type="submit" className="group relative w-full flex justify-center
-                py-2 px-4 border border-transparent text-sm font-bold
-                rounded-md text-white bg-indigo-600 hover:bg-indigo-700
-                focus:outline-none focus:ring-2 focus:ring-offset-2
-                focus:ring-black-500 space-y-4 ring">Login</button>
-                </form>
-            </div>
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
+  const { login } = useStore();
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    try {
+      const response = await login(username, password);
+      if (response.ok) {
+        history.push("/");
+      } 
+      else {
+        alert("Invalid username or password");
+      }
+    } catch (error) {
+      console.error(error);
+      alert("An error occurred while logging in");
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <form onSubmit={handleSubmit}>
+        <h1 className="text-3xl font-bold mb-4">Login</h1>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Username
+          </label>
+          <input
+            type="text"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
         </div>
-    );
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Password
+          </label>
+          <input
+            type="password"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
